44d2e4c3a57b3aaac60c9e7a3eb60fca4e0a20d7
okothmax <okothmaxwell03@gmail.com>
2023-11-02 13:41:46 +0300

    modified secrets generation script

--- backend/secret.py
+++ backend/secret.py
@@ -1,7 +1,9 @@
 import os
 import base64
 
-# Generate a random JWT secret of 256 bits (32 bytes)
-secret = base64.urlsafe_b64encode(os.urandom(32)).decode('utf-8')
+# Generate a random JWT and Cookie  secret of 256 bits (32 bytes)
+jwt_secret = base64.urlsafe_b64encode(os.urandom(32)).decode('utf-8')
+cookie_secret = base64.urlsafe_b64encode(os.urandom(32)).decode('utf-8')
+print(f"The jwt_secret is: {jwt_secret[:-2]}")
+print(f"The cookie_secret is: {cookie_secret[:-2]}")
 
-print(secret)
